50f63399a7ef44605027e8a05f267f3e4d63cfaf
Manuel Alejandro Ambriz Baca <manuel-alex-1@hotmail.com>
2017-12-14 17:23:36 -0600

    Initial Commit

---
+++ Neighbors AlertaVecinal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Neighbors AlertaVecinal
+//
+//  Created by Manuel Ambriz on 14/12/17.
+//  Copyright © 2017 Manuel Ambriz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
